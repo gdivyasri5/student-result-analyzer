43a17c8a53ce11d343878ed7568f1736caca30da
gdivyasri07 <167666940+gdivyasri07@users.noreply.github.com>
2026-02-05 17:56:34 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Student Result Analyzer
+
+A simple Python project built using Streamlit and deployed online.
---
+++ app.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+st.title("Student Result Analyzer")
+
+st.write("My first deployed Python project")
