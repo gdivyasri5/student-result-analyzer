9d9af112738570bf7d08d687b8070f9216de300a
gdivyasri07 <167666940+gdivyasri07@users.noreply.github.com>
2026-02-05 18:40:18 +0530

    Add student input feature

--- app.py
+++ app.py
@@ -2,4 +2,16 @@ import streamlit as st
 
 st.title("Student Result Analyzer")
 
-st.write("My first deployed Python project")
+students = {}
+
+name = st.text_input("Student Name")
+marks = st.number_input("Marks", min_value=0, max_value=100)
+
+if st.button("Add Student"):
+    if name:
+        students[name] = marks
+        st.success(f"Added {name} with marks {marks}")
+    else:
+        st.warning("Please enter student name")
+
+st.write("Current Students:", students)
